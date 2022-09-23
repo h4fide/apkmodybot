39e4a315e6391767d0d9e0b10eb87994ff512b85
h4fide <40579109+h4fide@users.noreply.github.com>
2022-09-23 22:05:54 +0100

    Update

--- apkmody.py
+++ apkmody.py
@@ -1,28 +1,59 @@
 import requests
 import bs4 as bs
 
+
+
 m_url = 'https://apkmody.io'
 
-class Mody():
+class Mody(): 
     def __init__(self):
         self.search = self.search
         self.items = self.items
         self.dlurl = self.dlurl
+
     def search(search):
         req = requests.get(f'{m_url}/?s={search}')
         soup = bs.BeautifulSoup(req.text, 'html.parser')
         data = []
         resultes = soup.find_all('div', class_='flex-item')
-        for result in resultes:
-            title = result.find('h2', class_='truncate').text
-            ver_m = result.find('p' , class_='card-excerpt has-small-font-size truncate').text
-            version = ver_m.replace('\n', '').split('•')[0]
-            mod = ver_m.replace('\n', '').split('•')[1][1:]
-            link =  result.find('a')['href']
-            image = result.find('img')['src']
-            data.append({'title': title, 'version': version, 'mod': mod, 'link': link, 'image': image})
-        return data
+        for result in resultes[:6]:
+            try: 
+                title = result.find('h2', class_='truncate').text
+                ver_m = result.find('p' , class_='card-excerpt has-small-font-size truncate').text
+                version = ver_m.replace('\n', '').split('•')[0]
+                mod = ver_m.replace('\n', '').split('•')[1][1:]
+                link =  result.find('a')['href']
+                image = result.find('img')['src']
+                size = Mody.get_size(link)
+                data.append({'title': title, 'version': version, 'mod': mod, 'link': link, 'image': image, 'size': size})
+            except:
+                pass
+        if data == []:
+            return None
+        else:
+            return data
 
+
+    def get_size(url):
+        try:
+            req = requests.get(url, timeout=10)
+            soup = bs.BeautifulSoup(req.text, 'html.parser')
+            results = soup.find_all('th', class_='has-text-align-left has-small-font-size has-cyan-bluish-gray-color')
+    
+            data = []
+            for result in results:
+                # where is the size 
+                if result.text == 'Size':
+                    size = result.find_next_sibling('td').text
+                    data.append(size)
+        except Exception as e:
+            print(e)
+            return 'Unknown'
+        if data == []:
+            return 'Unknown'
+        else:
+            return data[0]
+        
     def items(url):
         f_url ='https://download.apkmody.fun'
         req = requests.get(f'{url}/download', timeout=10)
@@ -30,9 +61,12 @@ class Mody():
         results = soup.find_all('a', class_='clickable')
         data = []
         for result in results[:-2][1:]:
-            link = result['href']
-            name = result.find('div', class_='has-vivid-cyan-blue-color').text
-            data.append({'name': name, 'link': f_url+link})
+            try:
+                link = result['href']
+                name = result.find('div', class_='has-vivid-cyan-blue-color').text
+                data.append({'name': name, 'link': f_url+link})
+            except:
+                pass
         return data
 
     def dlurl(url):
@@ -45,3 +79,13 @@ class Mody():
         return data[0]
 
 
+    def get_trending():
+        req = requests.get(m_url, timeout=10)
+        soup = bs.BeautifulSoup(req.text, 'html.parser')
+        results = soup.find_all('ul', class_='footer-menu')
+        data = []
+        for result in results[2:]:
+            trinding = result.find_all('li')
+            link = trinding[0].find('a')['href']
+            print(link)
+    
--- bot.py
+++ bot.py
@@ -1,39 +1,82 @@
 import os
-from time import sleep
-import telebot
-from telebot import types
-from confige import token
+try:
+    import telebot
+    from telebot import types
+    from pySmartDL import SmartDL
+except:
+    os.system("pip install pyTelegramBotAPI pySmartDL")
+
 from apkmody import Mody
+from config import token
 
 bot = telebot.TeleBot(token)
 
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.send_photo(message.chat.id, caption='Hi, I am apkmody Bot. \nI can search for apps on apkmody.io and download them for you. \nJust send me the name of the app you want to download ☕.', photo='https://apkmody.io/wp-content/uploads/2021/06/xapks-installer-cover-1440x480.jpg')
+
+
+
 @bot.message_handler(func=lambda message: True)
 def searching_mod(message):
-    if message.text.lower() == 'stop':
-        bot.send_message(message.chat.id, 'Bye')
-        bot.stop_bot()
+    wait = bot.send_message(message.chat.id, 'Searching...')
+    search = message.text
+    if Mody.search(search) == None:
+        bot.delete_message(message.chat.id, wait.message_id)
+        bot.send_message(message.chat.id, 'No results found')
     else:
-        search = message.text
-        wait = bot.send_message(message.chat.id, 'Searching...')
-        wait
         data = Mody.search(search)
         bot.delete_message(message.chat.id, wait.message_id)
         for i in data:
             markup = types.InlineKeyboardMarkup()
             btn_docs= types.InlineKeyboardButton(text='Link 🌍', url=i['link'])
-            markup.add(types.InlineKeyboardButton('Download ⚡', callback_data='download'), btn_docs)
-            bot.send_photo(message.chat.id, i['image'], caption=f'''*{i['title']}*
-*Version:* {i['version']}
-*Mod:* {i['mod']}
-''', parse_mode='Markdown', reply_markup=markup)
+            markup.add(types.InlineKeyboardButton('⚡', callback_data='download'), btn_docs)
+            bot.send_photo(message.chat.id, i['image'], caption=f'''
+            *{i['title']}*\n*Version:* {i['version']}\n*Mod:* {i['mod']}\n*Size:* {i['size']}'''
+            , parse_mode='Markdown', reply_markup=markup)
+
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'download')
 def choose_mod(call):
-    print(call)
+    url = call.message.reply_markup.keyboard[0][1].url
+    data = Mody.items(url)
+    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)        
+    for i in data:
+        markup = types.InlineKeyboardMarkup()
+        btn_docs= types.InlineKeyboardButton(text='Link 🌍', url=i['link'])
+        markup.add(types.InlineKeyboardButton('Download 🌠', callback_data='mod'), btn_docs)
+        bot.reply_to(call.message, f'''*{i['name']}* 📥''', parse_mode='Markdown', reply_markup=markup) 
 
 
-
+@bot.callback_query_handler(func=lambda call: call.data == 'mod')
+def download_mod(call):
+    c_url = call.message.reply_markup.keyboard[0][1].url
+    if c_url.count('//') == 2:
+        url = c_url.split('//')
+        url = url[0]+'//'+url[2]
+    elif c_url.count('//') == 1:
+        url = c_url
+    dlurl = Mody.dlurl(url)
+    filename = call.message.text.replace(' ','_').replace(' 📥','')
+    filename = f'{filename}_{call.message.message_id}.apk'
+    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
+    downloading = bot.reply_to(call.message, 'Downloading...')
+    obj = SmartDL(dlurl, dest=f'./{filename}', progress_bar=True)
+    obj.start()
+    if obj.isSuccessful() == True:
+        bot.delete_message(call.message.chat.id, downloading.message_id)
+        uplding = bot.reply_to(call.message, 'Uploading...')
+        try:
+            bot.send_document(call.message.chat.id, open(f'./{filename}', 'rb'))
+            bot.delete_message(call.message.chat.id, uplding.message_id)
+            os.remove(f'./{filename}')
+        except Exception as e:
+            bot.delete_message(call.message.chat.id, uplding.message_id)
+            bot.send_message(call.message.chat.id, f'Coudn\'t upload file, it\'s too big. Try to download it from link below\n{dlurl}')
+            os.remove(f'./{filename}')
+        bot.delete_message(call.message.chat.id, uplding.message_id)
+    
 
 
 bot.infinity_polling()
